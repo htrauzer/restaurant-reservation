7bb584a5618f65a2811093048bb259adb9184627
htrauzer <trauzer.g@gmail.com>
2026-03-18 19:36:08 +0200

    for tables layout

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO restaurant_table (capacity, zone, pos_x, pos_y) VALUES (2, 'MAIN_HALL', 100, 150);
+INSERT INTO restaurant_table (capacity, zone, pos_x, pos_y) VALUES (4, 'TERRACE', 300, 450);
+-- Додаємо всі типи столів згідно з вимогами
+
