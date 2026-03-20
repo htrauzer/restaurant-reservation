e7ed1935a1879e05f5c26c0c67f9b33eaf818b59
htrauzer <trauzer.g@gmail.com>
2026-03-20 13:39:56 +0200

    fixed Update stautus

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -11,24 +11,28 @@ export const MapUI = {
     },
 
     
-    updateStatus(occupiedIds, recommendedIds) {
-        document.querySelectorAll('.table-item').forEach(tableDiv => {
-            const id = parseInt(tableDiv.getAttribute('data-id'));
-            
-            
-            if (occupiedIds.includes(id)) {
-                tableDiv.style.backgroundColor = "#95a5a6";
-                tableDiv.classList.add('status-reserved');
-                tableDiv.style.pointerEvents = "none";
-            } 
-            
-            else if (recommendedIds.includes(id)) {
-                tableDiv.style.backgroundColor = "#f1c40f"; 
-                tableDiv.style.boxShadow = "0 0 15px rgba(241, 196, 15, 0.8)";
-            }
-        });
-    },
+   updateStatus(occupiedIds, recommendedIds) {
+    document.querySelectorAll('.table-item').forEach(tableDiv => {
+        const id = parseInt(tableDiv.getAttribute('data-id'));
+    
+        tableDiv.style.backgroundColor = "#2ecc71"; 
+        tableDiv.classList.remove('status-reserved');
+        tableDiv.style.boxShadow = "none";
+        tableDiv.style.pointerEvents = "auto";
+        tableDiv.style.border = "none";
 
+        if (occupiedIds.includes(id)) {
+            tableDiv.style.backgroundColor = "#95a5a6"; 
+            tableDiv.classList.add('status-reserved');
+            tableDiv.style.pointerEvents = "none"; 
+        } 
+     
+        else if (recommendedIds.includes(id)) {
+            tableDiv.style.backgroundColor = "#f1c40f"; 
+            tableDiv.style.boxShadow = "0 0 15px rgba(241, 196, 15, 0.8)";
+        }
+    });
+},
    
     highlightFound(tableIds) {
         tableIds.forEach(id => {
